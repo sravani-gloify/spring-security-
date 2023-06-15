9ff7191cb09a7d86d3f3d40070239c275a1d7bcd
sravani-gloify <akula.sravani@gloify.com>
2023-06-15 17:03:59 +0530

    first commit

---
+++ src/main/java/com/javatechie/dto/Product.java
@@ -0,0 +1,45 @@
+package com.javatechie.dto;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+	@Id
+    private int productId;
+    private String name;
+    private int qty;
+    private double price;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+}
